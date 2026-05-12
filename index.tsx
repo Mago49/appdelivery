8a6c1c3286fc9c79a95fffc70517ceb55d6c53ab
Mago49 <pedrimafc@gmail.com>
2026-05-12 03:07:32 -0300

    Atualizar o index.tsx

--- index.tsx
+++ index.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home"; // Página já existente (exemplo)
-import InventoryControl from "./pages/InventoryControl"; // Página a ser criada
-
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home} /> {/* Página inicial */}
-        <Route path="/inventory" component={InventoryControl} /> {/* Nova rota */}
+        {/* Rota inicial do sistema */}
+        <Route exact path="/" component={Home} />
+
+        {/* Nova página - Controle de Estoque */}
+        <Route path="/inventory" component={InventoryControl} />
       </Switch>
     </Router>
   );
